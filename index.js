c653dcb3b6dd2610b23e7c68e4ceb96976f66ef5
Marie-AnneS <marie-anne.seim@gmail.com>
2019-05-29 01:55:18 +0000

    exercise avec les class

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
-let balance = 500.00;
 
-class Withdrawal {
 
-  constructor(amount) {
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+}
+
+class Transaction /* extends Account */ {
+  constructor(amount, account) {
+    //super();
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
   }
-
 }
 
+class Deposit extends Transaction{
+get value(){
+  return this.amount;    
+  }
+}
 
+class Withdrawal extends Transaction {
+  get value(){
+    return -this.amount;    
+  }
+  
+}
+const myAccount = new Account('billybob');
 
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
+t1.commit();
+
+const t2 = new Withdrawal(50.00, myAccount);
+t2.commit();
+
+console.log('Ending Balance:', myAccount.balance);
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+/* t1 = new Withdrawal(50.25);
 t1.commit();
 console.log('Transaction 1:', t1);
 
@@ -26,4 +54,8 @@ t2 = new Withdrawal(9.99);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00);
+t3.commit();
+console.log('transaction 3:', t3);
+
+console.log('Balance:', balance); */
